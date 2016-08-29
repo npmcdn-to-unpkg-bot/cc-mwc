4567372860e4de5a4870bc1943a4d71596caff0f
Cyrus <csarkosh@uw.edu>
2016-08-29 12:17:25 -0700

    moved handleDisconnect() into lib/ directory

--- app.js
+++ app.js
@@ -4,37 +4,45 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
-// custome modules
+// custom modules
 var log = require('./lib/log');
+var handleDisconnect = require('./lib/handleDisconnect');
 var api = require('./api/api');
 
 // setup dependencies
 var app = express();
-var mysql_config = {
+var connection = handleDisconnect({
 	host: 'localhost',
 	user: 'root',
 	password: 'password',
 	database: 'ccmwc'
-}
-var connection;
-function handleDisconnect() {
-	connection = mysql.createConnection(mysql_config);
-	connection.connect(function(err) {
-		if (err) {
-			log(err, 'app');
-			setTimeout(handleDisconnect, 2000);
-		} 
-	});
-	connection.on('error', function(err) {
-		console.log('mysql error: ' + err, 'app');
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			handleDisconnect();
-		} else {
-			throw err;
-		}
-	})
-}
-handleDisconnect();
+});
+
+// var mysql_config = {
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'password',
+// 	database: 'ccmwc'
+// }
+// var connection;
+// function handleDisconnect() {
+// 	connection = mysql.createConnection(mysql_config);
+// 	connection.connect(function(err) {
+// 		if (err) {
+// 			log(err, 'app');
+// 			setTimeout(handleDisconnect, 2000);
+// 		} 
+// 	});
+// 	connection.on('error', function(err) {
+// 		console.log('mysql error: ' + err, 'app');
+// 		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+// 			handleDisconnect();
+// 		} else {
+// 			throw err;
+// 		}
+// 	})
+// }
+// handleDisconnect();
 
 // set global vars
 app.set('connection', connection);

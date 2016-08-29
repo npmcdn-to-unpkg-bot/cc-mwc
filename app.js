c44f136a1b7dd142baa15c52bfd3b5f785dd1560
Cyrus <csarkosh@uw.edu>
2016-08-29 12:18:50 -0700

    removed temp pword

--- app.js
+++ app.js
@@ -14,35 +14,8 @@ var app = express();
 var connection = handleDisconnect({
 	host: 'localhost',
 	user: 'root',
-	password: 'password',
 	database: 'ccmwc'
 });
-
-// var mysql_config = {
-// 	host: 'localhost',
-// 	user: 'root',
-// 	password: 'password',
-// 	database: 'ccmwc'
-// }
-// var connection;
-// function handleDisconnect() {
-// 	connection = mysql.createConnection(mysql_config);
-// 	connection.connect(function(err) {
-// 		if (err) {
-// 			log(err, 'app');
-// 			setTimeout(handleDisconnect, 2000);
-// 		} 
-// 	});
-// 	connection.on('error', function(err) {
-// 		console.log('mysql error: ' + err, 'app');
-// 		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-// 			handleDisconnect();
-// 		} else {
-// 			throw err;
-// 		}
-// 	})
-// }
-// handleDisconnect();
 
 // set global vars
 app.set('connection', connection);

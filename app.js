8a65175f1136b07edad1772672d2d52cbc75656f
Cyrus <csarkosh@uw.edu>
2016-09-04 18:51:24 -0700

    updated local imports to work with reformatted directory

--- app.js
+++ app.js
@@ -13,15 +13,18 @@
  * @desc Launches application on port 8080
  */
 
-// module dependencies
-var express = require('express');
+// dependencies
 var bodyParser = require('body-parser');
+var express = require('express');
+var fs = require('fs');
+var https = require('https');
 var mysql = require('mysql');
+var path = require('path');
 
 // custom modules
-var log = require('./lib/log');
-var handleDisconnect = require('./lib/handleDisconnect');
-var api = require('./api/api');
+var log = require('./src/log');
+var handleDisconnect = require('./src/handleDisconnect');
+var api = require('./src/api/api');
 
 // setup dependencies
 var app = express();
@@ -37,7 +40,7 @@ app.set('express', express);
 app.set('mysql', mysql);
 
 // middleware
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // api routes
 app.use('/api', api);
